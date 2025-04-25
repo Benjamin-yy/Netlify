fcbaf2a4d7b36c2509b1d8cf2bf269c15354fed3
FXY <702675033@qq.com>
2025-04-25 14:57:50 +0800

    six

---
+++ js/fundation.js
@@ -0,0 +1,346 @@
+var data = {
+    "name": "植物界",
+    "children": [{
+            "name": "被子植物门",
+            "children": [
+                {
+                    "name": "双子叶植物纲",
+                    "children": [
+                        {"name": "华盖木", "value": 15012,"image":'img/images/华盖木.png'},
+                        {"name": "滇桐", "value": 100,"image":'img/images/滇桐.jpg'},
+                        {"name": "闽楠", "value": 13000,"image":'img/images/闽楠.png'},
+                        {"name": "银缕梅", "value": 500,"image":'img/images/银缕梅.jpg'},
+                        {"name": "花榈木", "value": 1000,"image":'img/images/花榈木.jpg'},
+                        {"name": "红豆树", "value": 2000,"image":'img/images/红豆树.jpg'},
+                        {"name": "望天树", "value": 2000,"image":'img/images/望天树.jpg'},
+                        {"name": "金花茶", "value": 18000,"image":'img/images/金花茶.jpg'},
+                        {"name": "珙桐", "value": 1000,"image":'img/images/珙桐.jpg'},
+                        {"name": "喜树", "value": 5000,"image":'img/images/喜树.jpg'},
+                        {"name": "香果树", "value": 3000,"image":'img/images/香果树.png'},
+                        {"name": "太行花", "value": 500,"image":'img/images/太行花.jpg'},
+                        {"name": "茶梅", "value": 8000,"image":'img/images/茶梅.jpg'},
+                        {"name": "盐桦", "value": 50,"image":'img/images/盐桦.jpg'},
+                        {"name": "白菊木", "value": 8000,"image":'img/images/白菊木.jpg'},
+                        {"name": "牛皮杜鹃", "value": 3000,"image":'img/images/牛皮杜鹃.jpg'},
+                        {"name": "小花异裂菊", "value": 500,"image":'img/images/小花异裂菊.jpg'}
+                    ]
+                },
+                {
+                    "name": "单子叶植物纲",
+                    "children": [
+                        {"name": "美花兰", "value": 121,"image":'img/images/美花兰.jpg'},
+                        {"name": "文山红柱兰", "value": 211,"image":'img/images/文山红柱兰.jpg'},
+                        {"name": "琼棕", "value": 500,"image":'img/images/琼棕.jpg'},
+                        {"name": "乳头百合", "value": 2100,"image":'img/images/乳头百合.jpg'},
+                        {"name": "华山新麦草", "value": 2117,"image":'img/images/华山新麦草.jpg'},
+                        {"name": "大黄花虾脊兰", "value": 1017,"image":'img/images/大黄花虾脊兰.jpg'},
+                        {"name": "霍山石斛", "value": 97,"image":'img/images/霍山石斛.jpg'},
+                        {"name": "大百合", "value": 5017,"image":'img/images/大百合.jpg'}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "裸子植物门",
+            "children": [
+                {
+                    "name": "红豆杉纲",
+                    "children": [
+                        {"name": "东北红豆杉", "value": 11100,"image":'img/images/东北红豆杉.jpg'},
+                        {"name": "西藏红豆杉", "value": 9800,"image":'img/images/西藏红豆杉.jpg'},
+                        {"name": "喜马拉雅红豆杉", "value": 1300,"image":'img/images/喜马拉雅红豆杉.jpg'},
+                        {"name": "穗花杉", "value": 4325,"image":'img/images/穗花杉.jpg'},
+                        {"name": "台湾穗花杉", "value": 2100,"image":'img/images/台湾穗花杉.jpg'},
+                        {"name": "白豆杉", "value": 492,"image":'img/images/白豆杉.jpg'},
+                        {"name": "长叶榧树", "value":5210,"image":'img/images/长叶榧树.jpg'},
+                        {"name": "云南榧树", "value": 938,"image":'img/images/云南榧树.jpg'},
+                        {"name": "川滇红豆杉", "value": 5268,"image":'img/images/川滇红豆杉.jpg'},
+                        {"name": "灰岩红豆杉", "value": 150,"image":'img/images/灰岩红豆杉.jpg'}
+                    ]
+               },
+                {
+                    "name": "松柏纲",
+                    "children": [
+                        {"name": "银杉", "value": 12941,"image":'img/images/银杉.jpg'},
+                        {"name": "水杉", "value": 5294,"image":'img/images/水杉.jpg'},
+                        {"name": "金钱松", "value": 181,"image":'img/images/金钱松.jpg'}]
+                },
+                {
+                    "name": "买麻藤纲",
+                    "children": [
+                        {"name": "草麻黄", "value": 495,"image":'img/images/草麻黄.png'},
+                        {"name": "百岁兰", "value": 2136,"image":'img/images/百岁兰.jpg'}
+                    ]
+                },
+                {
+                    "name": "苏铁纲",
+                    "children": [
+                        {"name": "叉叶苏铁", "value": 194,"image":'img/images/叉叶苏铁.jpg'},
+                        {"name": "攀枝花苏铁", "value": 1294,"image":'img/images/攀枝花苏铁.jpg'},
+                        {"name": "秀叶苏铁", "value": 2294,"image":'img/images/秀叶苏铁.jpg'}
+                    ]
+                },
+                {
+                    "name": "银杏纲",
+                    "children": [
+                        {"name": "银杏", "value": 294,"image":'img/images/银杏.jpg'}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "绿藻门",
+            "children": [
+                {
+                    "name": "绿藻纲",
+                    "children": [
+                        {"name": "胶毛藻", "value": 10000,"image":'img/images/胶毛藻.jpg'},
+                        {"name": "刚毛藻", "value": 9800,"image":'img/images/刚毛藻.jpg'},
+                        {"name": "螺旋硬毛藻", "value": 500,"image":'img/images/螺旋硬毛藻.png'},
+                        {"name": "中华胶毛藻", "value": 175,"image":'img/images/中华胶毛藻.png'},
+                        {"name": "短刺卵囊藻", "value": 100,"image":'img/images/短刺卵囊藻.jpg'}
+                    ]
+                },
+                {
+                    "name": "石莼纲",
+                    "children": [
+                        {"name": "礁膜", "value": 49132,"image":'img/images/礁膜.jpg'},
+                        {"name": "羽藻", "value": 13214,"image":'img/images/羽藻.jpg'},
+                        {"name": "长柄浒苔", "value": 273,"image":'img/images/长柄浒苔.jpg'},
+                        {"name": "小齿礁膜", "value": 300,"image":'img/images/小齿礁膜.png'}
+                    ]
+                },
+                {
+                    "name": "轮藻纲",
+                    "children": [
+                        {"name": "脆轮藻", "value": 9487,"image":'img/images/脆轮藻.jpg'},
+                        {"name": "丽藻", "value": 14210,"image":'img/images/丽藻.jpg'},
+                        {"name": "稀少灯枝藻", "value": 120,"image":'img/images/稀少灯枝藻.jpg'}
+                    ]
+                },
+            ]
+        },
+        {
+            "name": "红藻门",
+            "children": [
+                {
+                    "name": "红藻纲",
+                    "children": [
+                        {"name": "珍珠麒麟菜", "value": 4865,"image":'img/images/珍珠麒麟菜.jpeg'},
+                        {"name": "石花菜", "value": 10000,"image":'img/images/石花菜.jpg'},
+                        {"name": "曾氏江蓠", "value": 215,"image":'img/images/曾氏江蓠.jpg'},
+                        {"name": "错综江蓠", "value": 465,"image":'img/images/错综江蓠.jpg'},
+                        {"name": "海萝", "value": 10000,"image":'img/images/海萝.png'},
+                        {"name": "珊瑚藻", "value": 21500,"image":'img/images/珊瑚藻.png'}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "蕨类植物门",
+            "children": [
+                {
+                    "name": "水韭纲",
+                    "children": [
+                        {"name": "中华水韭", "value": 4865,"image":'img/images/中华水韭.jpg'},
+                        {"name": "云贵水韭", "value": 2000,"image":'img/images/云贵水韭.jpg'}
+                    ]
+                },
+                {
+                    "name": "木贼纲",
+                    "children": [
+                        {"name": "中华木贼", "value": 213,"image":'img/images/中华木贼.jpg'},
+                        {"name": "问荆", "value": 2000,"image":'img/images/问荆.jpg'}
+                    ]
+                },
+                {
+                    "name": "真蕨纲",
+                    "children": [
+                        {"name": "桫椤", "value": 21300,"image":'img/images/桫椤.jpg'},
+                        {"name": "光叶蕨", "value": 95,"image":'img/images/光叶蕨.png'},
+                        {"name": "荷叶铁线蕨", "value": 600,"image":'img/images/荷叶铁线蕨.jpg'},
+                        {"name": "瓶儿小草", "value": 4000,"image":'img/images/瓶儿小草.png'},
+                        {"name": "玉龙蕨", "value": 1940,"image":'img/images/玉龙蕨.png'},
+                        {"name": "中华桫椤", "value": 8000,"image":'img/images/中华桫椤.jpg'},
+                        {"name": "带状瓶尔小草", "value": 45,"image":'img/images/带状瓶尔小草.jpg'},
+                        {"name": "原始观音座莲", "value": 50,"image":'img/images/原始观音座莲.png'}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "石松门",
+            "children": [
+                {
+                    "name": "石松纲",
+                    "children": [
+                        {"name": "蛇足石杉", "value": 10000,"image":'img/images/蛇足石杉.jpg'},
+                        {"name": "福氏马尾杉", "value": 4500,"image":'img/images/福氏马尾杉.jpg'}]
+                },
+                {
+                    "name": "卷柏纲",
+                    "children": [
+                        {"name": "东方卷柏", "value": 1000,"image":'img/images/东方卷柏.png'},
+                        {"name": "垫状卷柏", "value": 50000,"image":'img/images/垫状卷柏.png'}]
+                }
+            ]
+        },
+        {
+            "name": "真藓门",
+            "children": [
+                {
+                    "name": "泥炭藓亚纲",
+                    "children": [
+                        {"name": "中华泥炭藓", "value": 54865,"image":'img/images/中华泥炭藓.jpg'},
+                        {"name": "阔边泥炭藓", "value": 40000,"image":'img/images/阔边泥炭藓.jpg'},
+                        {"name": "拟尖叶泥炭藓", "value": 80000,"image":'img/images/拟尖叶泥炭藓.jpg'},
+                        {"name": "多纹泥炭藓", "value": 30000,"image":'img/images/多纹泥炭藓.jpg'}
+                    ]
+                },
+                {
+                    "name": "黑藓亚纲",
+                    "children": [
+                        {"name": "疣黑藓", "value": 985,"image":'img/images/疣黑藓.jpg'},
+                        {"name": "云南黑藓", "value": 2000,"image":'img/images/云南黑藓.jpg'},
+                        {"name": "密叶黑藓", "value": 3014,"image":'img/images/密叶黑藓.jpg'}
+                    ]
+                },
+                {
+                    "name": "真藓亚纲",
+                    "children": [
+                        {"name": "桧叶白发藓", "value": 15000,"image":'img/images/桧叶白发藓.jpg'},
+                        {"name": "高山金发藓", "value": 75632,"image":'img/images/高山金发藓.jpg'},
+                        {"name": "小翠云", "value": 10000,"image":'img/images/小翠云.jpg'}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "地钱门",
+            "children": [
+                {
+                    "name": "地钱纲",
+                    "children": [
+                        {"name": "浮苔", "value": 936,"image":'img/images/浮苔.jpg'},
+                        {"name": "东亚地钱", "value": 1975,"image":'img/images/东亚地钱.jpg'}
+                    ]
+                },
+                {
+                    "name": "叶苔纲",
+                    "children": [
+                        {"name": "钩毛叶苔", "value": 3254,"image":'img/images/钩毛叶苔.jpg'},
+                        {"name": "钝叶光萼苔", "value": 89,"image":'img/images/钝叶光萼苔.jpg'},
+                        {"name": "吉永鞭苔", "value": 599,"image":'img/images/吉永鞭苔.jpg'}
+                    ]
+                },
+                {
+                    "name": "裸蒴苔纲",
+                    "children": [
+                        {"name": "大角苔", "value": 3524,"image":'img/images/大角苔.jpg'}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "角苔门",
+            "children": [
+                {
+                    "name": "角苔纲",
+                    "children": [
+                        {"name": "中华角苔", "value": 150,"image":'img/images/中华角苔.jpg'},
+                        {"name": "皱叶奇角苔", "value": 20,"image":'img/images/皱叶奇角苔.jpg'},
+                        {"name": "平滑角苔", "value": 200,"image":'img/images/平滑角苔.jpg'}
+                    ]
+                },
+                {
+                    "name": "短角苔纲",
+                    "children": [
+                        {"name": "圆叶短角苔", "value": 50,"image":'img/images/圆叶短角苔.jpg'},
+                        {"name": "爪哇短角苔", "value": 54,"image":'img/images/爪哇短角苔.jpg'}
+                    ]
+                },
+                {
+                    "name": "泥炭角苔纲",
+                    "children": [
+                        {"name": "球茎泥炭角苔", "value": 124,"image":'img/images/球茎泥炭角苔.jpg'},
+                        {"name": "平滑泥炭角苔", "value": 500,"image":'img/images/平滑泥炭角苔.jpg'}
+                    ]
+                }
+            ]
+        },
+    ]
+};
+
+
+var myChart = echarts.init(document.getElementById('main'));
+var option;
+
+myChart.setOption(option = {
+    tooltip: {
+    trigger: 'item',
+    formatter: function(params) {
+        // 开始构建工具提示内容
+        let content = `
+            <div class="custom-tooltip">
+                <div class="current-node">${params.name}</div>
+        `;
+
+        // 如果是叶子节点（没有子节点），添加现存数量和图片
+        if (!params.data.children) {
+            content += `
+                <div class="leaf-content">
+                    <div>数量：<span class="value-tag">${params.data.value || 'N/A'}</span></div>
+            `;
+            if (params.data.image) {
+                content += `
+                    <div class="image-wrapper">
+                        <img src="${params.data.image}" class="node-image">
+                    </div>
+                `;
+            }
+            content += `
+                </div>
+            `;
+        }
+
+        // 关闭工具提示的 div
+        content += `
+            </div>
+        `;
+
+        return content;
+    }
+},
+    // 保持其他树图配置不变
+    series: [{
+        type: 'tree',
+        data: [data],
+        top: '1%',
+        left: '7%',
+        bottom: '1%',
+        right: '20%',
+        symbolSize: 7,
+        label: {
+            position: 'left',
+            verticalAlign: 'middle',
+            align: 'right',
+            fontSize: 14
+        },
+        leaves: {
+            label: {
+                position: 'right',
+                verticalAlign: 'middle',
+                align: 'left'
+            }
+        },
+        emphasis: {
+            focus: 'descendant'
+        },
+        expandAndCollapse: true,
+        animationDuration: 550,
+        animationDurationUpdate: 750,
+        initialTreeDepth: 1
+    }]
+});
+
+myChart.setOption(option);
